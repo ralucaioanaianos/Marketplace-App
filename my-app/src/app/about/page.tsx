09f0622c4a28538c0e308d5d38ed9bdf746908c5
ralucaioanaianos <raluca_ianos@yahoo.com>
2024-03-19 10:32:11 +0200

    final

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -3,7 +3,13 @@ import styles from './About.module.css';
 
 const About = () => {
   return (
-    <div>About page</div>
+    <div className={styles.aboutPage}>
+      <h1 className={styles.aboutTitle}>About this project</h1>
+      <div className={styles.aboutContent}>
+        <p>The Dream Closet website is a simple online marketplace. Users can list items they want to sell by filling out a form with details like the item name, photo name, gender, and available sizes. Once submitted, the item is added to a list of items available for sale.</p> 
+       <p>Overall, my project provides a basic framework for an online marketplace, allowing users to easily list items for sale. It is a user-friendly platform for buying and selling goods online.</p> 
+      </div>
+    </div>
   );
 }
 
